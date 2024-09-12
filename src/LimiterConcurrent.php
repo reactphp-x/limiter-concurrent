b1951913fba34be12d3aa8f53a66c15c0adf95aa
wpjscc <wpjscc@gmail.com>
2024-09-12 16:23:39 +0800

    add stream support

--- src/LimiterConcurrent.php
+++ src/LimiterConcurrent.php
@@ -9,24 +9,26 @@ final class LimiterConcurrent
 {
     protected TokenBucket $tokenBucket;
 
+    protected Concurrent $messageQueueConcurrent;
     protected Concurrent $concurrent;
 
     protected bool $isQueue;
 
 
-    public function __construct(int $perInterval, int | string $interval = 1000, bool $isQueue = false, int $maxLimit = 0)
+    public function __construct(int $perInterval, int | string $interval = 1000, bool $isQueue = false, int $maxLimit = 0, $stream = false)
     {
         $this->tokenBucket = new TokenBucket($perInterval, $perInterval, $interval);
-        $this->concurrent = new Concurrent(1, $maxLimit);
+        $this->concurrent = new Concurrent($perInterval, $maxLimit, $stream);
+        $this->messageQueueConcurrent = new Concurrent(1);
         $this->isQueue = $isQueue;
     }
 
     public function concurrent(callable $callback)
     {
         if ($this->isQueue) {
-            return $this->concurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1)->then($callback));
+            return $this->concurrent->concurrent(fn () => $this->messageQueueConcurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1)->then($callback)));
         }
-        return $this->concurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1))->then($callback);
+        return $this->concurrent->concurrent(fn () =>$this->messageQueueConcurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1))->then($callback));
     }
 
     public function tryAcquire(int $num = 1): bool
