8c4954ac5ea1c488005f6d515893b689f440cbc6
wpjscc <wpjscc@gmail.com>
2024-09-16 13:18:45 +0800

    update

--- src/LimiterConcurrent.php
+++ src/LimiterConcurrent.php
@@ -2,12 +2,12 @@
 
 namespace ReactphpX\LimiterConcurrent;
 
-use ReactphpX\Limiter\TokenBucket;
+use ReactphpX\Limiter\RateLimiter;
 use ReactphpX\Concurrent\Concurrent;
 
 final class LimiterConcurrent
 {
-    protected TokenBucket $tokenBucket;
+    protected RateLimiter $rateLimiter;
 
     protected Concurrent $messageQueueConcurrent;
     protected Concurrent $concurrent;
@@ -17,7 +17,7 @@ final class LimiterConcurrent
 
     public function __construct(int $perInterval, int | string $interval = 1000, bool $isQueue = false, int $maxLimit = 0, $stream = false)
     {
-        $this->tokenBucket = new TokenBucket($perInterval, $perInterval, $interval);
+        $this->rateLimiter = new RateLimiter($perInterval, $interval);
         $this->concurrent = new Concurrent($perInterval, $maxLimit, $stream);
         $this->messageQueueConcurrent = new Concurrent(1);
         $this->isQueue = $isQueue;
@@ -26,14 +26,26 @@ final class LimiterConcurrent
     public function concurrent(callable $callback)
     {
         if ($this->isQueue) {
-            return $this->concurrent->concurrent(fn () => $this->messageQueueConcurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1)->then($callback)));
+            return $this->concurrent->concurrent(fn () => $this->messageQueueConcurrent->concurrent(fn () => $this->rateLimiter->removeTokens(1)->then($callback)));
         }
-        return $this->concurrent->concurrent(fn () =>$this->messageQueueConcurrent->concurrent(fn () => $this->tokenBucket->removeTokens(1))->then($callback));
+        return $this->concurrent->concurrent(fn () =>$this->messageQueueConcurrent->concurrent(fn () => $this->rateLimiter->removeTokens(1))->then($callback));
     }
 
-    public function __call($method, $args)
+
+    public function tryRemoveTokens(int $count): bool
     {
-        return $this->tokenBucket->$method(...$args);
+        return $this->rateLimiter->tryRemoveTokens($count);
     }
+
+    public function getTokensRemaining(): int
+    {
+        return $this->rateLimiter->getTokensRemaining();
+    }
+
+    public function addTokens(int $count): void
+    {
+        $this->rateLimiter->addTokens($count);
+    }
+    
     
 }
